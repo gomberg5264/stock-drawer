53a82d1766300c13f8f3405ae62889061fe41aa2
Anthony Trinh <47074388+anthony-trinh@users.noreply.github.com>
2021-10-21 23:55:08 -0400

    Update Stock.js
    Converted chart from line chart, to candlesticks

--- src/Stock.js
+++ src/Stock.js
@@ -7,10 +7,18 @@ class Stock extends React.Component {
     */
 
     constructor(props) {
+        /*
+        Initialize state of component 
+
+        this.state.ticker = symbol/ticker representing the stock 
+        this.state.x_values = list containing values for x-axis of chart, i.e. dates 
+        this.state.y_values = dictionary containing lists, lists are: open, high, low, close 
+        */
         super(props) ; 
         this.state = { 
+            ticker: null,
             x_values: [],
-            y_values: []
+            y_values: {}
         }
     }
 
@@ -19,6 +27,9 @@ class Stock extends React.Component {
     }
 
     fetchStock(){
+        /*
+        Fetches a stock 
+        */
         const ticker = 'IBM' ;
         const api_key = '6GK1I729285WW00D' ;
         const instance = this ;
@@ -34,16 +45,27 @@ class Stock extends React.Component {
                 function(data){
 
                     let temp_x_values = [] ; 
-                    let temp_y_values = {} ;
+                    let temp_y_values = {
+                        "open": [], 
+                        "high": [], 
+                        "low": [], 
+                        "close": []
+                    } ;
 
                     console.log(data) ;
 
                     for(var key in data['Time Series (Daily)']){
                         temp_x_values.push(key) ;
-                        temp_y_values.push(data['Time Series (Daily)'][key]['1. open']) ;
+                        temp_y_values["open"].push(data['Time Series (Daily)'][key]['1. open']) ;
+                        temp_y_values["high"].push(data['Time Series (Daily)'][key]['2. high']) ;
+                        temp_y_values["low"].push(data['Time Series (Daily)'][key]['3. low']) ;
+                        temp_y_values["close"].push(data['Time Series (Daily)'][key]['4. close']) ;
                     }
+
+                    console.log(temp_y_values) ;
                     
                     instance.setState({
+                        ticker: ticker, 
                         x_values: temp_x_values, 
                         y_values: temp_y_values 
                     }) ;
@@ -59,13 +81,16 @@ class Stock extends React.Component {
               data={[
                 {
                   x: this.state.x_values,
-                  y: this.state.y_values,
-                  type: 'scatter',
+                  open: this.state.y_values["open"],
+                  high: this.state.y_values["high"],
+                  low: this.state.y_values["low"],
+                  close: this.state.y_values["close"],
+                  type: 'candlestick',
                   mode: 'lines+markers',
                   marker: {color: 'red'},
                 }
               ]}
-              layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
+              layout={{width: 720, height: 500, title: 'xD'}}
             />
           </div>
         )
